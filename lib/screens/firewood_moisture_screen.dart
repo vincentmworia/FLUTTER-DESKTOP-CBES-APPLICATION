3f025bd09fddd87fd4b657348d0d1edd08d37cc7
Vincent Mworia <mworiavince@gmail.com>
2023-02-07 17:53:45 +0300

    firewood -v3

--- lib/screens/firewood_moisture_screen.dart
+++ lib/screens/firewood_moisture_screen.dart
@@ -37,19 +37,19 @@ class FirewoodMoistureData extends StatefulWidget {
   final Map<String, dynamic> firewoodData;
 
   // final bool decompressNavPlane;
-
   @override
   State<FirewoodMoistureData> createState() => _FirewoodMoistureDataState();
 }
 
 class _FirewoodMoistureDataState extends State<FirewoodMoistureData> {
-
-  final _controller = TextEditingController();
+  final _searchController = TextEditingController();
+  final _stackNameController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    _searchController.dispose();
+    _stackNameController.dispose();
   }
 
   Map<String, dynamic> firewoodData = {};
@@ -60,50 +60,101 @@ class _FirewoodMoistureDataState extends State<FirewoodMoistureData> {
 
   @override
   Widget build(BuildContext context) {
-    if (_controller.text == "") {
+    if (_searchController.text == "") {
       firewoodData = widget.firewoodData;
     } else {
       // print(_controller.text);
       firewoodData = {};
       widget.firewoodData.forEach((key, value) {
-        if (key.contains(_controller.text)) {
+        if (key.contains(_searchController.text)) {
           firewoodData[key] = value;
         }
       });
     }
-    print(_pageData);
     return LayoutBuilder(builder: (_, cons) {
       return Stack(
         children: [
           ListView(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _controller,
-                  onChanged: (value) => setState(() {}),
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.all(20),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: const BorderSide(width: 0.8),
-                    ),
-                    hintText: 'Search stack number',
-                    suffixIcon: Padding(
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _controller.text = "";
-                            });
-                          },
-                          icon: const Icon(
-                            Icons.clear,
-                            size: 30,
-                          )),
+                      child: TextField(
+                        controller: _searchController,
+                        onChanged: (value) => setState(() {}),
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(20),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(width: 0.8),
+                          ),
+                          hintText: 'Search Stack by Id',
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _searchController.text = "";
+                                  });
+                                },
+                                icon: const Icon(
+                                  Icons.clear,
+                                  size: 30,
+                                )),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton.icon(
+                        icon: const Icon(Icons.add),
+                        onPressed: () async {
+                          await showDialog(
+                              context: context,
+                              builder: (ctx) => AlertDialog(
+                                    content: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: TextField(
+                                        controller: _stackNameController,
+                                        decoration: InputDecoration(
+                                          contentPadding:
+                                              const EdgeInsets.all(20),
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30),
+                                            borderSide:
+                                                const BorderSide(width: 0.8),
+                                          ),
+                                          hintText: 'Enter Stack ID',
+                                        ),
+                                      ),
+                                    ),
+                                    actions: [
+                                      ElevatedButton(
+                                          onPressed: () async {
+                                            if (widget.firewoodData.keys
+                                                .contains(_stackNameController
+                                                    .text)) {
+                                              print('The stack already exists');
+                                            } else {
+                                              print('DN#');
+                                            }
+                                            // print(_stackNameController.text);
+                                            // todo, should be a trial and error,
+                                            //  todo in case of any error, Display the error
+                                            // todo
+                                          },
+                                          child: const Text('Ok'))
+                                    ],
+                                  ));
+                        },
+                        label: const Text('Add Stack')),
+                  )
+                ],
               ),
               Wrap(
                 children: [
@@ -125,19 +176,41 @@ class _FirewoodMoistureDataState extends State<FirewoodMoistureData> {
                                     .colorScheme
                                     .primary
                                     .withOpacity(0.4),
-                                onTap: () {
+                                onTap: () async {
                                   setState(() {
                                     // _openPage = true;
                                     _pageData = {e: firewoodData[e]};
-                                    _op = 1.0;
+                                  });
+                                  await Future.delayed(
+                                      Duration(milliseconds: 100));
+                                  setState(() {
+                                    _op = 1;
                                   });
                                 },
-                                child: Center(
-                                    child: ListTile(
-                                  title: Text(e),
-                                  subtitle:
-                                      Text(firewoodData[e].length.toString()),
-                                )),
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: Align(
+                                        alignment: FractionalOffset.center,
+                                        child: Text(
+                                          e,
+                                          style: const TextStyle(
+                                              overflow: TextOverflow.fade,
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ),
+                                    // IconButton(
+                                    //     onPressed: () {},
+                                    //     icon: Icon(
+                                    //       Icons.delete,
+                                    //       color: Theme.of(context)
+                                    //           .colorScheme
+                                    //           .secondary,
+                                    //     ))
+                                  ],
+                                ),
                               ),
                             ),
                           ))
@@ -148,7 +221,7 @@ class _FirewoodMoistureDataState extends State<FirewoodMoistureData> {
           ),
           if (_pageData != null)
             AnimatedOpacity(
-                duration: const Duration(milliseconds: 300),
+                duration: const Duration(milliseconds: 500),
                 opacity: _op,
                 child: Container(
                   width: double.infinity,
@@ -160,70 +233,25 @@ class _FirewoodMoistureDataState extends State<FirewoodMoistureData> {
                         child: Text(_pageData.toString()),
                       ),
                       Positioned(
-                        top: 10,
-                        right: 10,
+                        top: 15,
+                        right: 15,
                         child: IconButton(
+                          iconSize: 30.0,
                           icon: Icon(
                             Icons.cancel,
                             color: Theme.of(context).colorScheme.secondary,
                           ),
-                          onPressed: () {
-                            setState(() {
-                              _pageData = null;
-                              // _openPage = false;
-                              _op = 0.0;
-                            });
+                          onPressed: () async {
+                            setState(() => _op = 0.0);
+                            await Future.delayed(
+                                const Duration(milliseconds: 500));
+                            setState(() => _pageData = null);
                           },
                         ),
                       ),
                     ],
                   ),
                 )),
-          // AnimatedContainer(
-          //   duration: const Duration(milliseconds: 300),
-          //   color: _stackPressed != -1
-          //       ? Theme.of(context).colorScheme.secondary.withOpacity(0.2)
-          //       : Colors.white,
-          //   height: double.infinity,
-          //   width: _stackPressed < 0 ? width : 0,
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //     children: [
-          //       GestureDetector(
-          //         onTap: () {
-          //           setState(() {
-          //             _stackPressed = 1;
-          //           });
-          //         },
-          //         child: Container(
-          //           width: 100,
-          //           height: 100,
-          //           color: Colors.red,
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // Expanded(
-          //     child: Stack(
-          //   children: [
-          //     Positioned(
-          //       top: 10,
-          //       right: 10,
-          //       child: IconButton(
-          //         icon: Icon(
-          //           Icons.cancel,
-          //           color: Theme.of(context).colorScheme.secondary,
-          //         ),
-          //         onPressed: () {
-          //           setState(() {
-          //             _stackPressed = -1;
-          //           });
-          //         },
-          //       ),
-          //     ),
-          //   ],
-          // ))
         ],
       );
     });
